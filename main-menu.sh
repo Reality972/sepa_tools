08b2ec4290c00e85d388fe51c645e2ea488a0c94
s.desmazon <sdesmazon@gmail.com>
2019-03-31 11:59:57 +0200

    v.1

--- main-menu.sh
+++ main-menu.sh
@@ -21,6 +21,9 @@ function BUILDRELEASE {
     echo -e "${CYAN}-               Generate Release                         -${SET}"
     echo -e "${CYAN}----------------------------------------------------------${SET}"
 
+    # establish branch and tag name variables
+    devBranch=develop
+
     # current Git branch
     branch=$(git symbolic-ref HEAD | sed -e 's,.*/\(.*\),\1,')
 
@@ -30,12 +33,19 @@ function BUILDRELEASE {
     # establish master branch name variables
     masterBranch=$branch
 
+    # Ensure working directory in version branch clean
+    git update-index -q --refresh
+    if ! git diff-index --quiet HEAD --; then
+        echo -e "\n\t- Working directory not clean, please commit your changes first ${RED}[KO]${SET}"
+        exit
+    fi
+
     # checkout to master branch, this will break if the user has uncommited changes
     git checkout $masterBranch
 
     # master branch validation
     if [ $branch = "master" ]; then
-        echo "Enter the release version number"
+        echo -e "${GREEN}Enter the release version number: ${SET}"
 
         read versionNumber
 
@@ -46,15 +56,11 @@ function BUILDRELEASE {
         releaseBranch=$versionNumber
         tagName=$versionLabel
 
-        echo "Started releasing $versionLabel for $projectName ....."
+        echo -e "${GREEN}Merge develop to master${SET}"
+        git merge --no-ff $devBranch
 
-        # Ensure working directory in version branch clean
-        git update-index -q --refresh
-        if ! git diff-index --quiet HEAD --; then
-        echo -e "\n\t- Working directory not clean, please commit your changes first ${RED}[KO]${SET}"
-        exit
-        fi
-
+        echo -e "${GREEN}Started releasing ${YELLOW}$versionLabel${SET} for ${YELLOW}$projectName${SET} .....${SET}"
+   
         # pull the latest version of the code from master
         git pull
 
@@ -105,23 +111,37 @@ function BUILDRELEASE {
     fi
 }
 
+function CLEANBRANCH {
+    echo -e "${CYAN}----------------------------------------------------------${SET}"
+    echo -e "${CYAN}-               Clean local branches                     -${SET}"
+    echo -e "${CYAN}----------------------------------------------------------${SET}"
+
+    # Prune remote branches
+    git remote prune origin 
+
+    # List local git branches
+    # Filter git branches down to only those with deleted upstream/remote counterparts
+    # Pluck out branch names from output
+    # Delete the branches
+    git branch -vv | grep 'origin/.*: gone]' | awk '{print $1}' | xargs git branch -d  
+}
+
 while [[ $REPLY != 0 ]]; do
     clear
-	echo -e "
+	echo -e "${CYAN}
     Please Select:
         1. Clean local branch Git with remote branch
         2. Start new branch
         3. Display Home Space Utilization
         4. Make a release version
-        0. Quit"
+        0. Quit${SET}"
 
     read -p "Enter selection : "
 
     if [[ $REPLY =~ ^[0-4]$ ]]; then
         # Clean local
         if [[ $REPLY == 1 ]]; then
-            echo "Hostname: $HOSTNAME"
-            uptime
+            CLEANBRANCH
             sleep $DELAY
         fi
         # Start branch
